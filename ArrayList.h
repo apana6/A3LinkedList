6ab7c67740bd30f6f8a82233a45e9336724454eb
Andrea Panaitidi <apanaitidi4031@sdsu.edu>
2026-09-21 15:06:37 -0700

    Refactor ArrayList to store pointers

--- ArrayList.h
+++ ArrayList.h
@@ -1,8 +1,67 @@
 //
 // Created by adapa on 9/21/2026.
-//
+#pragma once
 
-#ifndef A3LINKEDLIST_ARRAYLIST_H
-#define A3LINKEDLIST_ARRAYLIST_H
+#include <iostream>
+#include "List.h"
 
-#endif //A3LINKEDLIST_ARRAYLIST_H
+template <typename T>
+class ArrayList : public List<T> {
+
+public:
+    ArrayList():size_(0) {}
+    // Add to the front
+    void addFront(T* item) override{
+        if (size_ >= CAPACITY) {
+            std::cout << "ArrayList is full." << std::endl;
+            return;
+        }
+
+            for (int i = size_; i > 0; --i) {
+                data_[i] = data_[i - 1];
+            }
+
+            data_[0] = item;
+            ++size_;
+        }
+    void deleteFront() override{
+        if (size_ == 0) {
+            std::cout << "ArrayList is empty" << std::endl;
+            return;
+        }
+        delete data_[0];
+
+        for (int i = 0; i < size_ -1; ++i) {
+            data_[i] = data_[i + 1];
+        }
+        --size_;
+    }
+
+
+    bool search(T* value) const override{
+        for (int i = 0; i < size_; ++i) {
+            if (*data_[i] == *value) return true;
+        }
+        return false;
+            }
+        //time complexity - O(n)
+        //deleting from the back would be O(1)
+
+    void print() const override {
+        for (int i = 0; i < size_; ++i) {
+            std::cout << *data_[i] << ",";
+            }
+        std::cout << std::endl;
+        }
+
+    ~ArrayList() override {
+        for (int i = 0; i < size_; ++i) {
+            delete data_[i];
+        }
+    }
+
+    private:
+        static const int CAPACITY = 20;
+        T* data_[CAPACITY];
+        int size_;
+    };
